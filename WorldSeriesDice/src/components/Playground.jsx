a0d29996b8406012bc703f2793516bf6472cd41b
Behçet <behcetmuhammed>
2024-10-19 20:37:10 +0300

    v2

--- WorldSeriesDice/src/components/Playground.jsx
+++ WorldSeriesDice/src/components/Playground.jsx
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { GiBackForth } from "react-icons/gi";
 import Tooltip from "./Tooltip.jsx"; // Tooltip bileşenini içe aktar
 import "../css/playground.css";
+import "../css/header.css";
+
+// Zar resimleri
+import dice1 from "../image/dice1.png";
+import dice2 from "../image/dice2.png";
+import dice3 from "../image/dice3.png";
+import dice4 from "../image/dice4.png";
+import dice5 from "../image/dice5.png";
+import dice6 from "../image/dice6.png";
 
 function Playground() {
-  const [playerOne, setPlayerOne] = useState("Player1");
-  const [playerTwo, setplayerTwo] = useState("Player2");
+  const [playerOne, setPlayerOne] = useState("Player1 [Siz]");
+  const [playerTwo, setplayerTwo] = useState("Player2 [PC]");
   const [inputValue, setInputValue] = useState("");
+  const [isRolling, setIsRolling] = useState(false);
+  let intervalId; // Aralık ID
+
+  /*Zarlar*/
+  const [diceOne, setDiceOne] = useState(1); // İlk zarın başlangıç değeri
+  const [diceTwo, setDiceTwo] = useState(1); // İkinci zarın başlangıç değeri
+
+  useEffect(() => {
+    return () => {
+      // bileşen kaldırıldığında intervali temizle
+      clearInterval(intervalId);
+    };
+  }, []);
 
   const focus = () => {
     setPlayerOne("");
@@ -17,8 +39,64 @@ function Playground() {
     setInputValue(x);
   };
 
+  // Zarları render etme fonksiyonu
+  const renderDice = (diceValue) => {
+    switch (diceValue) {
+      case 1:
+        return <img src={dice1} alt="Zar 1" />;
+      case 2:
+        return <img src={dice2} alt="Zar 2" />;
+      case 3:
+        return <img src={dice3} alt="Zar 3" />;
+      case 4:
+        return <img src={dice4} alt="Zar 4" />;
+      case 5:
+        return <img src={dice5} alt="Zar 5" />;
+      case 6:
+        return <img src={dice6} alt="Zar 6" />;
+      default:
+        return null;
+    }
+  };
+
+  //Butona basıldığı zaman
+  const clickButton = () => {
+    setIsRolling(true);
+
+    // Zarları döndürmeye başla
+    intervalId = setInterval(() => {
+      setDiceOne(getRandomNumber());
+      setDiceTwo(getRandomNumber());
+    }, 100); // 100 ms aralıklarla zar değerlerini güncelle
+
+    // 3 saniye sonra zarları durdur ve rastgele bir değer al
+    setTimeout(() => {
+      clearInterval(intervalId); // döngüyü durdur
+      setDiceOne(getRandomNumber());
+      setDiceTwo(getRandomNumber());
+      setIsRolling(false);
+    }, 3000);
+  };
+
+  //1 ile 6 arasında Randum sayı
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  //Kazanan
+  function winning() {
+    if (diceOne > diceTwo) {
+      console.log("Kazandın");
+    } else if (diceOne === diceTwo) {
+      console.log("Berabere");
+    } else {
+      console.log("Kaybettin");
+    }
+  }
+
   return (
     <div className="middle-area">
+      {/* Player Text */}
       <div className="player-area">
         <Tooltip text="İsminizi değiştirmek için tıklayınız!">
           <div>
@@ -43,13 +121,18 @@ function Playground() {
           </div>
         </Tooltip>
       </div>
-      <div className="player-area">
-        <div>oyun alanı 1</div>
-        <div>oyun alanı 2</div>
+      {/* Game Area */}
+      <div className="game-area">
+        <div className="dice-area">
+          {renderDice(diceOne)}
+          {renderDice(diceTwo)}
+          {winning()}
+        </div>
       </div>
-      <div className="button-area">
+      {/* Button */}
+      <button onClick={clickButton} className="button-area">
         <GiBackForth className="button-icon" />
-      </div>
+      </button>
     </div>
   );
 }
